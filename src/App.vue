7a9ba8b06dcfcda83a208eca494fd551dcef525a
François Blin <frablin@gmail.com>
2021-03-24 19:42:13 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="nav" class="flex  justify-center  text-lg  font-bold">
+    <router-link to="/">Home</router-link>
+    <span class="mx-1">|</span>
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
